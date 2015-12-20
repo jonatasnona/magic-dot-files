55a333b3798f14d6c29c367f2e573db9b31ebc60
Jonatas Pedraza <jonatas.nona@gmail.com>
2015-12-20 17:47:39 -0200

    add openssh deps

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install aptitude vim git git-core gitk tig build-essential \
-    automake autotools puppet-lint pep8 pylint lacheck python-pip \
-    libxml2-utils
+    automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
+    openssh-client openssh-server
