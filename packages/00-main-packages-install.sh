22bacbeb89109ed3712fe71978f86839015c97b6
Jonatas Pedraza <jonatas.nona@gmail.com>
2015-12-21 11:07:38 -0200

    add gparted dep

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install aptitude vim git git-core gitk tig build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
-    openssh-client openssh-server vagrant
+    openssh-client openssh-server vagrant gparted
