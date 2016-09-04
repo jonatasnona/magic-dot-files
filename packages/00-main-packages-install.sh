1bd25b5e8f8f146769c4b128a66fcd949884cf78
Jonatas Pedraza <jonatas.pedraza@nokia.com>
2016-09-04 12:31:08 -0300

    add new packages

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install aptitude vim git git-core gitk tig build-essential \
+sudo apt-get install aptitude vim git git-core build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
-    openssh-client openssh-server vagrant gparted
+    openssh-client openssh-server vagrant gparted wireshark k3b ffmpeg \
+    htop
