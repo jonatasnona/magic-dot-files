dfbdddef1d24b1b90c57590fff40bbca00d99e66
Jonatas Pedraza <jonatas.pedraza@nokia.com>
2016-09-07 00:09:05 -0300

    add traceroute

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -3,4 +3,4 @@
 sudo apt-get install aptitude vim git git-core build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
     openssh-client openssh-server vagrant gparted wireshark k3b ffmpeg \
-    htop
+    htop traceroute
