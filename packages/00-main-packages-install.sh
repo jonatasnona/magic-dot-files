9052c6bff6810b46511da732c39f34b0f235d2ab
Jonatas Pedraza <jonatas.nona@gmail.com>
2017-01-01 22:51:29 -0400

    add httpstat

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -4,3 +4,5 @@ sudo apt-get install aptitude vim git git-core build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
     openssh-client openssh-server vagrant gparted wireshark k3b ffmpeg \
     htop traceroute mtr
+
+sudo pip install httpstat
