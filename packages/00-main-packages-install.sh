39001aab7ba597bd456994b74f6c61f4fa1dd648
Jonatas Pedraza <jonatas.nona@gmail.com>
2017-01-01 22:51:51 -0400

    add nmap

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -3,6 +3,6 @@
 sudo apt-get install aptitude vim git git-core build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
     openssh-client openssh-server vagrant gparted wireshark k3b ffmpeg \
-    htop traceroute mtr
+    htop traceroute mtr nmap
 
 sudo pip install httpstat
