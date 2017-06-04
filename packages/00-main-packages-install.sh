6621e3af83a2e35608d3d13f9207973c69e9ab91
Jonatas Pedraza <jonatas.nona@gmail.com>
2017-06-04 09:57:42 -0300

    add more essentials packages

--- packages/00-main-packages-install.sh
+++ packages/00-main-packages-install.sh
@@ -3,6 +3,8 @@
 sudo apt-get install aptitude vim git git-core build-essential \
     automake puppet-lint pep8 pylint lacheck python-pip libxml2-utils \
     openssh-client openssh-server vagrant gparted wireshark k3b ffmpeg \
-    htop traceroute mtr nmap
+    htop traceroute mtr nmap nload lxc axel chromium-browser vlc neovim rar
 
-sudo pip install httpstat
+pip install httpstat mycli
+
+vagrant plugin install mutate
--- packages/00-nodejs-packages-install.sh
+++ packages/00-nodejs-packages-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm install -g handlebars jshint jade-lint eslint jslint jsonlint js-yaml
+npm install -g handlebars jshint jade-lint eslint jslint jsonlint js-yaml vtop http-server
